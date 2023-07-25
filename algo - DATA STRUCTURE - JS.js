73f2e52ef0bcc67fdf0ef22bd07b0514c4fdf274
Ahmeddouiri <133807465+Ahmeddouiri@users.noreply.github.com>
2023-07-25 21:17:45 +0100

    Create algo - DATA STRUCTURE - JS.js

---
+++ algo - DATA STRUCTURE - JS.js
@@ -0,0 +1,15 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let set1=[5,6,8,2]
+let set2=[2,8,6,4]
+
+for(x=0;x < set1.length ; x++ )
+{
+for(y=0;y < set2.length ; y++)
+{
+   if(set1[x]) != set2[y])
+   {
+     occ=
+   }
+}
+}
